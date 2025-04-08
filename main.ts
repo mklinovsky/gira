db94b1f5e61e3b5fac4133aaf424378f39f02ebf
mklinovsky <michal.klinovsky@gmail.com>
2025-04-08 22:58:08 +0200

    feat(jira-api): Add cliffy

--- main.ts
+++ main.ts
@@ -1,5 +1,30 @@
-import { JiraClient } from "./src/jira-api.ts";
-import { IssueStatus } from "./src/jira.types.ts";
+import * as JiraClient from "./src/jira/jira-api.ts";
+import { Command } from "@cliffy/command";
 
-const jiraApi = new JiraClient();
+async function main() {
+  await new Command()
+    .name("jirant")
+    .version("0.0.1")
+    .description("Jira CLI tool, for creating and managing issues")
+    .command("create", "Create a new issue")
+    .arguments("<summary:string>")
+    .option("-p, --parent <parent:string>", "Parent issue key")
+    .option("-i, --issue <issue:string>", "Issue type", {
+      default: "Task",
+    })
+    .action(
+      async (options: { issue: string; parent: string }, summary: string) => {
+        const issueKey = await JiraClient.createIssue(
+          summary,
+          options.issue,
+          options.parent,
+        );
+        console.log(`Created issue: ${issueKey}`);
+      },
+    )
+    .parse(Deno.args);
+}
 
+if (import.meta.main) {
+  await main();
+}
---
+++ src/jira/jira-api.ts
@@ -0,0 +1,96 @@
+import { requireEnv } from "../utils/utils.ts";
+import {
+  CreateIssuePayload,
+  IssueStatus,
+  IssueStatusById,
+} from "./jira.types.ts";
+
+const API_TOKEN = requireEnv("JIRA_API_TOKEN");
+const USER_EMAIL = requireEnv("JIRA_USER_EMAIL");
+const USER_ID = requireEnv("JIRA_USER_ID");
+const BASE_URL = requireEnv("JIRA_URL");
+const PROJECT_KEY = requireEnv("JIRA_PROJECT_KEY");
+
+const apiUrl = `${BASE_URL}/rest/api/3`;
+
+export async function createIssue(
+  summary: string,
+  issueName = "Task",
+  parentIssueKey?: string,
+): Promise<string> {
+  const payload: CreateIssuePayload = {
+    fields: {
+      project: { key: PROJECT_KEY },
+      assignee: { id: USER_ID },
+      summary,
+      issuetype: { name: issueName },
+      ...(parentIssueKey ? { parent: { key: parentIssueKey } } : {}),
+    },
+  };
+
+  const { key } = await postJson<CreateIssuePayload, { key: string }>(
+    `${apiUrl}/issue`,
+    payload,
+  );
+
+  console.log(`Created issue: ${BASE_URL}/browse/${key}`);
+  return key;
+}
+
+export async function changeIssueStatus(
+  issueKey: string,
+  statusId: IssueStatus,
+) {
+  const payload = { transition: { id: statusId } };
+
+  const url = `${apiUrl}/issue/${issueKey}/transitions`;
+  await postJson<typeof payload>(url, payload);
+
+  console.log(
+    `Changed status of issue ${issueKey} to ${IssueStatusById[statusId]}`,
+  );
+}
+
+function getHeaders(): HeadersInit {
+  return {
+    Authorization: `Basic ${btoa(`${USER_EMAIL}:${API_TOKEN}`)}`,
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+}
+
+function getRequestOptions<Payload>(payload: Payload): RequestInit {
+  return {
+    method: "POST",
+    headers: getHeaders(),
+    body: JSON.stringify(payload),
+  };
+}
+
+async function postJson<Payload, Response = unknown>(
+  url: string,
+  payload: Payload,
+): Promise<Response> {
+  try {
+    const response = await fetch(url, getRequestOptions(payload));
+
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+
+    if (response.status === 204) {
+      return {} as Response;
+    }
+
+    const data = await response.json();
+
+    if (data.errorMessages?.length || data.errors) {
+      throw new Error(`${data.errorMessages} ${data.errors}`);
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Request failed:", error);
+    throw error;
+  }
+}
