448b33171e96142875978f51d90cfda309d74018
mklinovsky <michal.klinovsky@gmail.com>
2025-07-05 13:52:01 +0200

    fix: build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,7 @@ version=$(jq -r '.version' ./deno.json)
 
 rm -rf ./dist
 mkdir -p ./dist
+cd ./dist
 
 targets=(
   "x86_64-unknown-linux-gnu"
@@ -16,7 +17,7 @@ targets=(
 
 for target in "${targets[@]}"; do
   echo "Compiling for $target..."
-  binary_file="dist/gira-$version-$target"
+  binary_file="gira-$version-$target"
 
   deno compile --allow-env --allow-sys --allow-read --allow-net --output "$binary_file" --target "$target" src/main.ts
 
